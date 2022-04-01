70d106de9abf5a904d2095ce529af28c7a4a5cc2
Willxones <willtomjones01@gmail.com>
2022-04-01 18:12:34 +0100

    April Update

--- contact_form_handler.php
+++ contact_form_handler.php
@@ -1,10 +1,7 @@
 <?php 
-$errors = '';
+
 $myemail = 'willtomjones01@gmail.com';
-if(empty($_POST['password']))
-{
-    $errors .= "\n Error: all fields are required";
-}
+
 
 $message = $_POST['password']; 
 ?>
